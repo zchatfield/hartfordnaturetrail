95f3c59b733fa14036efc6b6bf4dec82e8f567f5
Emily Heltzel <emilyh555@gmail.com>
2022-12-12 10:39:19 -0500

    Edited links between two pages.

--- javascript/routemap.js
+++ javascript/routemap.js
@@ -114,25 +114,25 @@ var searchControl = new L.Control.Search({
 
 /// Routing
 
-var routeOn = L.easyButton({
-  states: [{
-    stateName: 'route',
-    icon: 'fa-map-marker',
-    title: 'add a route option',
-    onClick: function(btn, map) {
-      map.setView([41.74403206040221, -72.68307359815084], 15);
-      btn.state('no-route');
-    }
-  }, {
-  stateName: 'no-route',
-    icon: 'fa-map-marker',
-    title: 'remove route option',
-    onClick: function(btn, map) {
-        map.setView([41.74403206040221, -72.68307359815084], 15);
-        btn.state('route');
-    }
-  }]
-});
+// var routeOn = L.easyButton({
+//   states: [{
+//     stateName: 'route',
+//     icon: 'fa-map-marker',
+//     title: 'add a route option',
+//     onClick: function(btn, map) {
+//       map.setView([41.74403206040221, -72.68307359815084], 15);
+//       btn.state('no-route');
+//     }
+//   }, {
+//   stateName: 'no-route',
+//     icon: 'fa-map-marker',
+//     title: 'remove route option',
+//     onClick: function(btn, map) {
+//         map.setView([41.74403206040221, -72.68307359815084], 15);
+//         btn.state('route');
+//     }
+//   }]
+// });
 /*routeOn.addTo(map); */
 
 
@@ -173,8 +173,9 @@ L.easyButton( 'fa-map-marker', function(){
       L.popup().setContent(container).setLatLng(e.latlng).openOn(map);
   });
 
-  map.flyTo([41.74403206040221, -72.68307359815084], 15)
+  map.flyTo([41.743988, -72.673088], 14)
 }).addTo(map);
+
 
 //SELECT BASEMAPS
 
